554741e876f64bea67dca74f0b913103cc5839f4
Wiktor Zawadzki <wiktoz05@wp.pl>
2022-10-23 21:03:40 +0200

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import { SessionProvider } from "next-auth/react"
 import '../styles/globals.css'
+import ShopLayout from "../components/layout/shop"
+import AdminLayout from "../components/layout/admin"
+import { ShoppingCartProvider } from "../context/ShoppingCart"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps }, router }) {
+  const getLayout =
+        router.pathname.includes('/admin') ? ((page) => <AdminLayout children={page} />)
+        : ((page) => <ShopLayout children={page} />);
+  
+  return (
+    <>
+      {<SessionProvider session={session}><ShoppingCartProvider>{getLayout(<Component {...pageProps} />, pageProps)}</ShoppingCartProvider></SessionProvider>}
+    </>
+  )
 }
 
 export default MyApp
